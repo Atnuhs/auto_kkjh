1c2cfcd5c4ef9c50e25fb82d8ff2bc53ec8a7bd7
Shunta Abe <color59abe@gmail.com>
2021-04-29 21:05:53 +0900

    make exe

--- app/cx-Freeze/setup.py
+++ app/cx-Freeze/setup.py
@@ -1,7 +1,7 @@
 import sys
 from cx_Freeze import setup, Executable
 
-
+build_exe_options = {"packages": [], "excludes": ["test"]}
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
@@ -10,5 +10,6 @@ setup(
     name="auto_kkjh",
     version="0.1",
     description="My GUI application!",
+    options={"build_exe": build_exe_options},
     executables=[Executable("../app.py", base=base)],
 )
